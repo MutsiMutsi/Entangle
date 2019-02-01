04b131a852a7b4cbd93657ef06bddde1f76eb2ea
Mutsi <mitchel.disveld@hotmail.com>
2019-02-01 22:59:53 +0100

    initial commit

---
+++ Entangle/CryptoHelper.cs
@@ -0,0 +1,301 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockchain
+{
+	public class CryptoHelper
+	{
+		public static string ToSHA256(object data)
+		{
+			StringBuilder sb = new StringBuilder();
+			SHA256 mySHA256 = SHA256Managed.Create();
+			if (data as byte[] != null)
+			{
+				foreach (Byte b in data as byte[])
+					sb.Append(b.ToString("x2"));
+				return sb.ToString();
+			}
+
+			BinaryFormatter bf = new BinaryFormatter();
+			using (MemoryStream ms = new MemoryStream())
+			{
+				bf.Serialize(ms, data);
+				byte[] hashBytes = mySHA256.ComputeHash(ms.ToArray());
+
+				foreach (Byte b in hashBytes)
+					sb.Append(b.ToString("x2"));
+
+				return sb.ToString();
+			}
+		}
+
+		public static byte[] ToSHA256Bytes(object data)
+		{
+			SHA256 mySHA256 = SHA256Managed.Create();
+			if (data as byte[] != null)
+			{
+				return mySHA256.ComputeHash(data as byte[]);
+			}
+			BinaryFormatter bf = new BinaryFormatter();
+			using (MemoryStream ms = new MemoryStream())
+			{
+				bf.Serialize(ms, data);
+				return mySHA256.ComputeHash(ms.ToArray());
+			}
+		}
+
+
+		public static byte[] HashAndSignBytes(byte[] DataToSign, RSAParameters Key)
+		{
+			try
+			{
+				// Create a new instance of RSACryptoServiceProvider using the 
+				// key from RSAParameters.  
+				RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+
+				RSAalg.ImportParameters(Key);
+
+				// Hash and sign the data. Pass a new instance of SHA1CryptoServiceProvider
+				// to specify the use of SHA1 for hashing.
+				return RSAalg.SignData(DataToSign, new SHA256CryptoServiceProvider());
+			}
+			catch (CryptographicException e)
+			{
+				Console.WriteLine(e.Message);
+
+				return null;
+			}
+		}
+
+		public static byte[] StringToByteArray(String hex)
+		{
+			int NumberChars = hex.Length;
+			byte[] bytes = new byte[NumberChars / 2];
+			for (int i = 0; i < NumberChars; i += 2)
+				bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+			return bytes;
+		}
+
+		public static bool VerifySignedHash(byte[] DataToVerify, byte[] SignedData, RSAParameters Key)
+		{
+
+			string toPem = CryptoHelper.PublicPEM(Key);
+			try
+			{
+				// Create a new instance of RSACryptoServiceProvider using the 
+				// key from RSAParameters.
+				RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+				RSAalg.ImportParameters(Key);
+
+				byte[] hashed = CryptoHelper.ToSHA256Bytes(DataToVerify);
+
+				string hexStrSign = Encoding.UTF8.GetString(SignedData);
+				byte[] sign2 = SignedData;// StringToByteArray(hexStrSign);
+
+				string byteToHex = BitConverter.ToString(sign2).Replace("-", string.Empty).ToLower();
+
+				bool t3 = RSAalg.VerifyData(DataToVerify, new SHA256CryptoServiceProvider(), sign2);
+				bool t5 = RSAalg.VerifyHash(hashed, CryptoConfig.MapNameToOID("SHA256"), sign2);
+
+				bool verified = t3 && t5;
+
+				// Verify the data using the signature.  Pass a new instance of SHA1CryptoServiceProvider
+				// to specify the use of SHA1 for hashing.
+				return verified;
+
+			}
+			catch (CryptographicException e)
+			{
+				Console.WriteLine(e.Message);
+
+				return false;
+			}
+		}
+
+		public static BigInteger FromBigEndian(byte[] p)
+		{
+			Array.Reverse(p);
+			if (p[p.Length - 1] > 127)
+			{
+				Array.Resize(ref p, p.Length + 1);
+				p[p.Length - 1] = 0;
+			}
+			return new BigInteger(p);
+		}
+
+		public static string PrivatePEM(RSAParameters parameters)
+		{
+			using (var stream = new MemoryStream())
+			{
+				var writer = new BinaryWriter(stream);
+				writer.Write((byte)0x30); // SEQUENCE
+				using (var innerStream = new MemoryStream())
+				{
+					var innerWriter = new BinaryWriter(innerStream);
+					EncodeIntegerBigEndian(innerWriter, new byte[] { 0x00 }); // Version
+					EncodeIntegerBigEndian(innerWriter, parameters.Modulus);
+					EncodeIntegerBigEndian(innerWriter, parameters.Exponent);
+					EncodeIntegerBigEndian(innerWriter, parameters.D);
+					EncodeIntegerBigEndian(innerWriter, parameters.P);
+					EncodeIntegerBigEndian(innerWriter, parameters.Q);
+					EncodeIntegerBigEndian(innerWriter, parameters.DP);
+					EncodeIntegerBigEndian(innerWriter, parameters.DQ);
+					EncodeIntegerBigEndian(innerWriter, parameters.InverseQ);
+					var length = (int)innerStream.Length;
+					EncodeLength(writer, length);
+					writer.Write(innerStream.GetBuffer(), 0, length);
+				}
+
+				string base64 = Convert.ToBase64String(stream.GetBuffer(), 0, (int)stream.Length);
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine("-----BEGIN RSA PRIVATE KEY-----");
+				// Output as Base64 with lines chopped at 64 characters
+				for (var i = 0; i < base64.Length; i += 64)
+				{
+					sb.AppendLine(base64.Substring(i, Math.Min(64, base64.Length - i)));
+					//outputStream.WriteLine(base64, i, Math.Min(64, base64.Length - i));
+				}
+				sb.AppendLine("-----END RSA PRIVATE KEY-----");
+
+				return sb.ToString().Replace("\\", "\\\\");
+			}
+		}
+
+		public static RSAParameters ReadPEM(string pemstr)
+		{
+			StringReader sr = new StringReader(pemstr);
+			PemReader pr = new PemReader(sr);
+
+			var KeyParameter = (Org.BouncyCastle.Crypto.AsymmetricKeyParameter)pr.ReadObject();
+			return DotNetUtilities.ToRSAParameters((RsaKeyParameters)KeyParameter);
+		}
+
+		public static string PublicPEM(RSAParameters parameters)
+		{
+			using (var stream = new MemoryStream())
+			{
+				var writer = new BinaryWriter(stream);
+				writer.Write((byte)0x30); // SEQUENCE
+				using (var innerStream = new MemoryStream())
+				{
+					var innerWriter = new BinaryWriter(innerStream);
+					innerWriter.Write((byte)0x30); // SEQUENCE
+					EncodeLength(innerWriter, 13);
+					innerWriter.Write((byte)0x06); // OBJECT IDENTIFIER
+					var rsaEncryptionOid = new byte[] { 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x01 };
+					EncodeLength(innerWriter, rsaEncryptionOid.Length);
+					innerWriter.Write(rsaEncryptionOid);
+					innerWriter.Write((byte)0x05); // NULL
+					EncodeLength(innerWriter, 0);
+					innerWriter.Write((byte)0x03); // BIT STRING
+					using (var bitStringStream = new MemoryStream())
+					{
+						var bitStringWriter = new BinaryWriter(bitStringStream);
+						bitStringWriter.Write((byte)0x00); // # of unused bits
+						bitStringWriter.Write((byte)0x30); // SEQUENCE
+						using (var paramsStream = new MemoryStream())
+						{
+							var paramsWriter = new BinaryWriter(paramsStream);
+							EncodeIntegerBigEndian(paramsWriter, parameters.Modulus); // Modulus
+							EncodeIntegerBigEndian(paramsWriter, parameters.Exponent); // Exponent
+							var paramsLength = (int)paramsStream.Length;
+							EncodeLength(bitStringWriter, paramsLength);
+							bitStringWriter.Write(paramsStream.GetBuffer(), 0, paramsLength);
+						}
+						var bitStringLength = (int)bitStringStream.Length;
+						EncodeLength(innerWriter, bitStringLength);
+						innerWriter.Write(bitStringStream.GetBuffer(), 0, bitStringLength);
+					}
+					var length = (int)innerStream.Length;
+					EncodeLength(writer, length);
+					writer.Write(innerStream.GetBuffer(), 0, length);
+				}
+
+				string base64 = Convert.ToBase64String(stream.GetBuffer(), 0, (int)stream.Length);
+				StringBuilder sb = new StringBuilder();
+				sb.AppendLine("-----BEGIN PUBLIC KEY-----");
+				// Output as Base64 with lines chopped at 64 characters
+				for (var i = 0; i < base64.Length; i += 64)
+				{
+					sb.AppendLine(base64.Substring(i, Math.Min(64, base64.Length - i)));
+					//outputStream.WriteLine(base64, i, Math.Min(64, base64.Length - i));
+				}
+				sb.AppendLine("-----END PUBLIC KEY-----");
+
+				return sb.ToString();
+			}
+		}
+
+		private static void EncodeLength(BinaryWriter stream, int length)
+		{
+			if (length < 0) throw new ArgumentOutOfRangeException("length", "Length must be non-negative");
+			if (length < 0x80)
+			{
+				// Short form
+				stream.Write((byte)length);
+			}
+			else
+			{
+				// Long form
+				var temp = length;
+				var bytesRequired = 0;
+				while (temp > 0)
+				{
+					temp >>= 8;
+					bytesRequired++;
+				}
+				stream.Write((byte)(bytesRequired | 0x80));
+				for (var i = bytesRequired - 1; i >= 0; i--)
+				{
+					stream.Write((byte)(length >> (8 * i) & 0xff));
+				}
+			}
+		}
+
+		private static void EncodeIntegerBigEndian(BinaryWriter stream, byte[] value, bool forceUnsigned = true)
+		{
+			stream.Write((byte)0x02); // INTEGER
+			var prefixZeros = 0;
+			for (var i = 0; i < value.Length; i++)
+			{
+				if (value[i] != 0) break;
+				prefixZeros++;
+			}
+			if (value.Length - prefixZeros == 0)
+			{
+				EncodeLength(stream, 1);
+				stream.Write((byte)0);
+			}
+			else
+			{
+				if (forceUnsigned && value[prefixZeros] > 0x7f)
+				{
+					// Add a prefix zero to force unsigned if the MSB is 1
+					EncodeLength(stream, value.Length - prefixZeros + 1);
+					stream.Write((byte)0);
+				}
+				else
+				{
+					EncodeLength(stream, value.Length - prefixZeros);
+				}
+				for (var i = prefixZeros; i < value.Length; i++)
+				{
+					stream.Write(value[i]);
+				}
+			}
+
+		}
+	}
+}
---
+++ Entangle/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Tangle.Net.Entity;
+using Tangle.Net.Repository;
+using Tangle.Net.Utils;
+using System.Security.Cryptography;
+using Blockchain;
+
+namespace Entangle
+{
+	class Node
+	{
+		public RSAParameters PublicKey;
+		public RSAParameters PrivateKey;
+		public string Chunk = "";
+
+		public Node()
+		{
+			RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+			PrivateKey = RSAalg.ExportParameters(true);
+			PublicKey = RSAalg.ExportParameters(false);
+		}
+
+		public string PublicKeyString
+		{
+			get
+			{
+				return Blockchain.CryptoHelper.PublicPEM(PublicKey);
+			}
+		}
+	}
+
+	class Program
+	{
+		const int CHUNK_SIZE_BYTES = 128;
+
+		static RestIotaRepository repository;
+
+		static byte[] GetHash(string input)
+		{
+			HashAlgorithm algorithm = SHA256.Create();
+			return algorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+		}
+
+		public static string GetHashString(string input)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (byte b in GetHash(input))
+				sb.Append(b.ToString("X2"));
+
+			return sb.ToString();
+		}
+
+		static void Main(string[] args)
+		{
+			int currentEpoch = 0;
+			Node node1 = new Node();
+			Node node2 = new Node();
+			// We send chunk to a trusted node
+			node1.Chunk = "0x1337EncryptedChunk";
+
+			// Nodes propagate data autonomously 
+			// Suggestion for incentivized propegation through financial incentive and relief of burden =>
+			// 1. Nodes can opt freely to propagate chunks further but do not have to do so
+			// 2. Nodes can only propegate data that is of the current epoch, when data is propegated a record is made on the tangle so that 
+			// the client can find the new node and to prove transfer was of the current epoch, all illegal propegation can therefor be skipped.
+			// the node will go through normal storage pipeline with other nodes and will thus pay for the chunks itself that it may/maynot store itself.
+			// accepting a lot of contracts even though you past through can be beneficial for the node because it improves its metrics of storage and reputation
+			// client is responsible to periodically verify enough nodes are storing the data
+			// The client can derive a new temporal salt with an iterator if rebroadcast must be done in the current epoch temporalSalt = hash(seed + iterator + currentepoch)
+
+			//propegation happens in current epoch
+			node2.Chunk = node1.Chunk;
+
+			//Epoch passes
+			currentEpoch++;
+
+			//Client wants proof of node1 and any possible neighbours (recursive n depth search with heuristics?)
+			//Two nodes are found to hold the data
+
+			//A client generates a sequential deterministic hash we will call TemporalSalt[Seed0,Seed1,SeedN]
+			//with the storage handle (decryption key) functioning as a seed 
+			//for every node to ask for proof of storage you compute a unique hash from the combined TemporalSalt and Node PublicKey called MixSalt.
+			//First get temporal salt for current epoch
+			string seed = "myHandle";
+			string temporalSalt = GetHashString(seed + currentEpoch);
+
+			string node1MixedSalt = GetHashString(temporalSalt + node1.PublicKeyString);
+			string node2MixedSalt = GetHashString(temporalSalt + node2.PublicKeyString);
+
+			//Mixed salts are posted on the iota tangle with the public node address as tag + epoch (later through iota smart contract invocation ?)
+			//The node finds a new transcation for this epoch with his public address, the node knows it has to provide proof of storage
+
+			//the node creates hash Proof = hash(chunk + mixedsalt)
+			string node1Proof = GetHashString(node1.Chunk + node1MixedSalt);
+			string node2Proof = GetHashString(node2.Chunk + node2MixedSalt);
+
+			//Nodes sign proof
+			byte[] node1ProofSignature = CryptoHelper.HashAndSignBytes(UTF8Encoding.UTF8.GetBytes(node1Proof), node1.PrivateKey);
+			byte[] node2ProofSignature = CryptoHelper.HashAndSignBytes(UTF8Encoding.UTF8.GetBytes(node2Proof), node2.PrivateKey);
+
+			// Nodes send iota transaction with their mixed salt as hash so that client can find the proof
+			// Nodes include their public key in the transaction
+			// Client finds the proofs and verifies storage
+			bool node1Verified = CryptoHelper.VerifySignedHash(UTF8Encoding.UTF8.GetBytes(node1Proof), node1ProofSignature, node1.PublicKey);
+			bool node2Verified = CryptoHelper.VerifySignedHash(UTF8Encoding.UTF8.GetBytes(node2Proof), node2ProofSignature, node2.PublicKey);
+
+
+			//Proposal for payments without contracts:
+			/*
+			 * Client pays whichever amount of nodes that have provided with proof based on how much redundancy is required.
+			 * nodes that do not receive payments simply drop the data, to prevent freeloading nodes could choose to not serve the data before payment for that epoch.
+			 * /
+
+
+
+
+			// ====== ------ Tangle Storage Test Application  ------ ========
+
+			//RestIotaRepository opacityTest = new RestIotaRepository(new RestClient("https://prodiota1.opacitynodes.com:14265"));//
+			//var testA = opacityTest.GetNodeInfo();
+			//string A = JsonConvert.SerializeObject(testA);
+
+			////opacityTest.
+			//var res = opacityTest.GetLatestInclusion(new List<Hash>());
+			////var testB = opacityTest.GetNeighbors();
+			////string B = JsonConvert.SerializeObject(testB);
+
+
+
+			//repository = new RestIotaRepository(new RestClient("http://node.deviceproof.org:14265"));
+			///*			
+			//var nodeInfo = repository.GetNodeInfo();
+			//var neighbours = repository.GetNeighbors();
+			//*/
+
+			//byte[] raw = GetFile("C:\\Users\\Mutsi\\Desktop\\oystericon_idle.png");
+			//List<Byte[]> chunks = GetChunks(raw).ToList();
+			////StitchAndSave(chunks);
+
+			//List<Hash> hashes = new List<Hash>();
+			//AttachChunk(chunks.First());
+			///*
+			//Parallel.ForEach(chunks, (chunk) =>
+			//{
+			//	hashes.Add(AttachChunk(chunk));
+			//});
+			//foreach (byte[] chunk in chunks)
+			//{
+			////	hashes.Add(AttachChunk(chunk));
+			//}*/
+		}
+
+		static byte[] GetFile(string path)
+		{
+			return File.ReadAllBytes(path);
+		}
+
+		static IEnumerable<byte[]> GetChunks(byte[] rawData)
+		{
+			List<byte[]> chunks = new List<byte[]>();
+
+			int taken = 0;
+			for (int i = 0; i < rawData.Length; i += CHUNK_SIZE_BYTES)
+			{
+				int remaining = Math.Min(CHUNK_SIZE_BYTES, rawData.Length - 1 - i);
+				chunks.Add(rawData.Skip(i).Take(remaining).ToArray());
+
+			}
+			return chunks;
+		}
+
+		static void StitchAndSave(IEnumerable<byte[]> chunks)
+		{
+			byte[] ret = new byte[chunks.Sum(x => x.Length)];
+			int offset = 0;
+			foreach (byte[] data in chunks)
+			{
+				Buffer.BlockCopy(data, 0, ret, offset, data.Length);
+				offset += data.Length;
+			}
+
+			File.WriteAllBytes("./test.png", ret);
+		}
+
+		static Hash AttachChunk(byte[] chunk)
+		{
+			var bundle = new Bundle();
+			bundle.AddTransfer(new Transfer
+			{
+				Address = new Address(Hash.Empty.Value),
+				Tag = new Tag("MUTSI"),
+				Timestamp = Timestamp.UnixSecondsTimestamp,
+				Message = TryteString.FromBytes(chunk)
+			});
+
+			bundle.Finalize();
+			bundle.Sign();
+
+			// see Getting Started to see how a repository is created
+			var result = repository.SendTrytes(bundle.Transactions);
+
+			return bundle.Hash;
+		}
+	}
+}
